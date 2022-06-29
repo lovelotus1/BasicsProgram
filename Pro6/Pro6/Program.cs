feea55ea6079c4a4858887b75c56e3a26c282cb6
lovelotus <opera.prem@gmail.com>
2022-06-29 08:27:53 +0530

    convert temp F to C

---
+++ Pro6/Pro6/Program.cs
@@ -0,0 +1,25 @@
+﻿//C# program to convert a Fahrenheit temperature into Celsius.
+
+using System;
+
+class Program
+{
+    static double convertToCelsius(double ferTemp)
+    {
+        double cTemp = 0;
+
+        cTemp = (ferTemp - 32) * 5 / 9;
+        return cTemp;
+    }
+    static void Main(string[] args)
+    {
+        double celTemp = 0;
+        double ferTemp = 0;
+
+        Console.Write("Enter the value of temperature in Fahrenheite(°F): ");
+        ferTemp = double.Parse(Console.ReadLine());
+
+        celTemp = convertToCelsius(ferTemp);
+        Console.WriteLine("Celsius temperature is(°C) : " + celTemp);
+    }
+}
